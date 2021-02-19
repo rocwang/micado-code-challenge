f56a32e39964905075b06949e776f4dd02cad766
Roc Wang <roc@kiwiberry.nz>
2021-02-19 15:23:01 +1300

    feat: basic error handling on queries

--- index.ts
+++ index.ts
@@ -22,11 +22,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", async (req, res) => {
-  const rows = await query(
-    (req.query.sql as string) ?? "select * from covid_19_new_zealand limit 10;"
-  );
+  let result: any;
 
-  res.json(rows);
+  try {
+    result = await query(req.query.sql as string);
+  } catch (error: any) {
+    result = error.toString();
+  }
+
+  res.json(result);
 });
 
 app.listen(port, () => {
