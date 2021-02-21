f946e66942082e858503053dbd04c29ca070c212
Roc Wang <roc@kiwiberry.nz>
2021-02-21 16:16:38 +1300

    feat: build the Docker image of node backend

--- backend/index.ts
+++ backend/index.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 import pg from "pg";
 
 const { Client } = pg;
+const port = 80;
 
 async function query(sql: string): Promise<object[]> {
   const client = new Client({
-    database: "micado",
-    port: 5432,
-    user: "covid_app",
+    host: process.env.HOST,
+    database: process.env.DB,
+    port: process.env.PORT ? parseInt(process.env.PORT) : undefined,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
   });
   await client.connect();
 
@@ -23,8 +26,6 @@ async function query(sql: string): Promise<object[]> {
 const app = express();
 app.use(cors());
 app.use(compression());
-
-const port = 3080;
 
 app.get("/", async (req, res) => {
   let result: any;
